01bcee8bc770d68e636504fe9838821ec29afc25
FirstProject <sutariyaavani30@gmail.com>
2023-08-16 15:32:47 -0400

    Changes are made in Cartpage as per naveenTL review comments

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -1,9 +1,17 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class CartPage {
 	
 	
-	String product="macbook";
+	By product=By.id("productId");
+	
+	public void clickProduct()
+	{
+		System.out.println("Click on macbook");
+	}
+	
 	
 	
 
