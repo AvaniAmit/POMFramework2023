088f22386f90f2ad0e115b6212313f9552f7b23c
FirstProject <sutariyaavani30@gmail.com>
2023-08-17 13:14:17 -0400

    order page code

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,16 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+	
+	public void order()
+	{
+		int i=10;
+		int p=20;
+		
+		System.out.println(i +"and" +p);
+		String n="Avani";
+		System.out.println(n);
+		
+	}
+
+}
